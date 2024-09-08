738d591fc95455da9e12ff3d808d796697d40c33
zach-conrad <zconrad119@gmail.com>
2024-09-08 16:21:41 -0500

    Updated mobile menu close button.

--- src/routes/components/header.svelte
+++ src/routes/components/header.svelte
@@ -1,102 +1,138 @@
 <script>
-    let isMenuOpen = false;
-    import { fly } from 'svelte/transition';
+	let isMenuOpen = false;
+	import { fly, fade } from 'svelte/transition';
 </script>
 
 <!--Main Navbar-->
 <header class="sticky top-0 z-50 bg-white shadow-lg font-roboto">
-    <nav class="container mx-auto p-4 flex justify-between items-center">
-        <div class="text-1xl font-bold z-50">
-            Zach <span class="text-blue-500">Conrad</span>
-        </div>
+	<nav class="container mx-auto p-4 flex justify-between items-center">
+		<div class="text-1xl font-bold z-50">
+			Zach <span class="text-blue-500">Conrad</span>
+		</div>
 
-        <ul class="hidden md:flex space-x-10">
-            <li><a href="#about" class="hover:text-gray-400">About</a></li>
-            <li><a href="#projects" class="hover:text-gray-400">Projects</a></li>
-            <li><a href="#work" class="hover:text-gray-400">Work</a></li>
-        </ul>
+		<ul class="hidden md:flex space-x-10">
+			<li><a href="#about" class="hover:text-gray-400">About</a></li>
+			<li><a href="#projects" class="hover:text-gray-400">Projects</a></li>
+			<li><a href="#work" class="hover:text-gray-400">Work</a></li>
+		</ul>
 
-        <!--Mobile Navbar (Hamburger to X)-->
-        <button class="md:hidden focus:outline-none z-50" on:click={() => isMenuOpen = !isMenuOpen}>
-            <div class="hamburger-icon">
-                <span class="line top" class:open={isMenuOpen}></span>
-                <span class="line middle" class:open={isMenuOpen}></span>
-                <span class="line bottom" class:open={isMenuOpen}></span>
-            </div>
-        </button>
-    </nav>
+		<!--Mobile Navbar (Hamburger to X)-->
+		<button class="md:hidden focus:outline-none z-50" on:click={() => (isMenuOpen = !isMenuOpen)}>
+			<div class="hamburger-icon">
+				<span class="line top" class:open={isMenuOpen}></span>
+				<span class="line middle" class:open={isMenuOpen}></span>
+				<span class="line bottom" class:open={isMenuOpen}></span>
+			</div>
+		</button>
+	</nav>
 
-    <!--Mobile menu-->
-    {#if isMenuOpen}
-    <ul class="md:hidden p-4 space-y-20 fixed top-0 left-0 w-full min-h-screen bg-white font-roboto font-bold flex flex-col justify-center items-start z-40">
-        <li><a href="#about" class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg" on:click={() => isMenuOpen = false} transition:fly={{duration: 500, x: -500}}>About</a></li>
-        <li><a href="#projects" class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg" on:click={() => isMenuOpen = false} transition:fly={{duration: 600, x: -500}}>Projects</a></li>
-        <li><a href="#work" class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg" on:click={() => isMenuOpen = false} transition:fly={{duration: 700, x: -500}}>Work</a></li>
-    </ul>
-    {/if}
+	<!--Mobile menu-->
+	{#if isMenuOpen}
+		<div class="menu" in:fade={{ duration: 300 }}>
+			<ul
+				class="md:hidden p-4 space-y-20 fixed top-0 left-0 w-full min-h-screen bg-white font-roboto font-bold flex flex-col justify-center items-start z-40"
+			>
+				<li>
+					<a
+						href="#about"
+						class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg"
+						on:click={() => (isMenuOpen = false)}
+						transition:fly={{ duration: 500, x: -500 }}>About</a
+					>
+				</li>
+				<li>
+					<a
+						href="#projects"
+						class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg"
+						on:click={() => (isMenuOpen = false)}
+						transition:fly={{ duration: 600, x: -500 }}>Projects</a
+					>
+				</li>
+				<li>
+					<a
+						href="#work"
+						class="block hover:text-gray-400 text-2xl text-left pl-6 drop-shadow-lg"
+						on:click={() => (isMenuOpen = false)}
+						transition:fly={{ duration: 700, x: -500 }}>Work</a
+					>
+				</li>
+			</ul>
+		</div>
+	{/if}
 </header>
 
 <style>
-    header {
-        background: #ffffff;
-        box-shadow: 100px;
-        padding: 20px;
-        border-bottom: solid 1px #ddd;
-    }
+	header {
+		background: #ffffff;
+		box-shadow: 100px;
+		padding: 20px;
+		border-bottom: solid 1px #ddd;
+	}
 
-    :global(html) {
-        scroll-behavior: smooth;
-    }
+	:global(html) {
+		scroll-behavior: smooth;
+	}
 
-    .hamburger-icon {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        width: 40px;
-        height: 24px;
-        cursor: pointer;
-        position: relative;
-        z-index: 50;
-    }
+	.menu {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: white;
+		z-index: 49;
+		transition: opacity 0.3s ease-in-out;
+	}
 
-    .line {
-        background-color: black;
-        height: 2px;
-        width: 36px;
-        transition: all 0.3s ease-in-out;
-        position: absolute;
-        transform-origin: center;
-    }
+	.hamburger-icon {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		width: 40px;
+		height: 24px;
+		cursor: pointer;
+		position: relative;
+		z-index: 50;
+	}
 
-    /* Set initial positions for top and bottom lines */
-    .top {
-        top: 0;
-    }
+	.line {
+		background-color: black;
+		height: 2px;
+		width: 36px;
+		transition: all 0.3s ease-in-out;
+		position: absolute;
+		transform-origin: center;
+	}
 
-    .middle {
-        top: 50%;
-        transform: translateY(-50%);
-    }
+	/* Set initial positions for top and bottom lines */
+	.top {
+		top: 0;
+	}
 
-    .bottom {
-        bottom: 0;
-    }
+	.middle {
+		top: 50%;
+		transform: translateY(-50%);
+	}
 
-    /* Rotate and animate into "X" */
-    :global(.open.top) {
-        transform: translateY(11px) rotate(45deg);
-    }
+	.bottom {
+		bottom: 0;
+	}
 
-    :global(.open.middle) {
-        opacity: 0;
-    }
+	/* Rotate and animate into "X" */
+	:global(.open.top) {
+		transform: translateY(11px) rotate(45deg);
+	}
 
-    :global(.open.bottom) {
-        transform: translateY(-11px) rotate(-45deg);
-    }
+	:global(.open.middle) {
+		opacity: 0;
+	}
 
-    /* Z-index of the full menu should be lower than the button */
-    ul {
-        z-index: 40;
-    }
+	:global(.open.bottom) {
+		transform: translateY(-11px) rotate(-45deg);
+	}
+
+	/* Z-index of the full menu should be lower than the button */
+	ul {
+		z-index: 40;
+	}
 </style>
