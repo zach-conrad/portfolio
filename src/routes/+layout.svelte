fd8debfe732a24295c5d1494f860d94759a92623
Zach-Conrad <zconrad119@gmail.com>
2025-06-04 21:24:17 -0500

    Analytics tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
 	import CursorGlow from './components/cursorGlow.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	injectAnalytics();
 </script>
 
 <CursorGlow />
